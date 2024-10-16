94f8f531d7af7226582789d4576a4bf9cd071be4
Kaushal2029 <117632711+Kaushal2029@users.noreply.github.com>
2024-10-16 17:24:17 +0530

    Inital Commit

---
+++ src/Form.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Button from "./Button";
+
+const Form = ({ reqType, setReqType }) => {
+  return (
+    <div>
+      <form
+        action=""
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <Button buttonText="users" reqType={reqType} setReqType={setReqType} />
+        <Button buttonText="posts" reqType={reqType} setReqType={setReqType} />
+        <Button buttonText="comments" reqType={reqType} setReqType={setReqType} />
+      </form>
+    </div>
+  );
+};
+
+export default Form;
